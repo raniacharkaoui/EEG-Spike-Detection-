2a7e6bc9906a2d7719536578e579863b6866f1e7
raniacharkaoui <48803445+raniacharkaoui@users.noreply.github.com>
2021-05-02 02:01:00 +0200

    English comments

--- Check_Same_SpikeDetected.m
+++ Check_Same_SpikeDetected.m
@@ -1,5 +1,8 @@
-function [A,B] = Check_Same_SpikeDetected(C,D,threshold, nb_min_same_pics)
+% This function counts the number of channels that detect the same spikes between a very small time interval 
+% (i.e nb_min_same_spike).
+% If the spike is detected nb_min_same_spike times on the channels then it's considered to be a 'true' spike
 
+function [A,B] = Check_Same_SpikeDetected(C,D,threshold, nb_min_same_spike)
     A = C;
     B = D;
     l = size(A);
@@ -8,63 +11,65 @@ function [A,B] = Check_Same_SpikeDetected(C,D,threshold, nb_min_same_pics)
     already_modified_index_length = 0;
 
     i = 1;
-    while i <= l(1)-1 %on compare chaque élément avec ceux qui le suivent et donc le dernier n'est pas nécessaire
-        % on fait cette comparaison que si l'élément est non nul
-        % et que l'élément n'appartient pas déjà à un groupe
+    while i <= l(1)-1%each element is compared with those that follow it, so the last one is not necessary
+        % this comparison is only made if the element is non-null
+        % and the element does not already belong to a group
         if A(i) ~= 0 && ~ismember(i,already_modified_index) 
-            nb_same_pics = 1; % le nombre de pics appartenant au groupe
+            nb_same_spike = 1; % the number of spikes belonging to the group
             next_channel_index = get_next_ch_index(A,i);
             nb_same_pics_diff_channels = 1; 
-            same_pics_indexes = [i]; % les index des éléments qui appartiennent au groupe
-
-            % on va regarder tous les éléments qui le suivent pour voir s'il
-            % peut appartenir au groupe
+            same_pics_indexes = [i]; % index of elements belonging to the group 
+            
+            %We will look at all the elements that follow it to see if it
+            % can belong to the group
             for j=i+1:l(1)
-                % Pour pouvoir appartenir au groupe, il faut être non nul, que
-                % les pics soit détectés dans la même fenêtre de temps et que
-                % l'élément n'appartienne pas déjà à un groupe
+                % In order to belong to the group, it is necessary to be
+                % non-null, necessary that
+                % the spikes are detected in the same time window and
+                % the element does not already belong to a group
 
                 if A(j) ~= 0 && is_same(A, i, j, threshold) && ~ismember(j,already_modified_index)
-                    % ajout de pic : on ajoute l'élément au groupe
-                    nb_same_pics = nb_same_pics + 1;
-                    same_pics_indexes(nb_same_pics) = j;
+                    % adding a spike : we add the element to the group 
+                    nb_same_spike = nb_same_spike + 1;
+                    same_pics_indexes(nb_same_spike) = j;
                     if j > next_channel_index
                         nb_same_pics_diff_channels = nb_same_pics_diff_channels + 1;
                     end 
                     
                 end
             end
-
-            % Il faut maintenant voir s'il y a assez d'éléments dans le groupe
-            % S'il y en a assez, 
-            % S'il n'y en a pas assez, on peut supprimer l'élément i qui a  
-            % généré le groupe. Les autres éléments du groupe ne sont pas
-            % supprimés car ils pourraient potentiellement appartenir à un
-            % autre groupe.
-            if nb_same_pics_diff_channels >= nb_min_same_pics % si le pic est suffisamment détecté
-                % assigner toutes les valeurs du groupe à la moyenne du
-                % groupe.
-                % Il faut aussi ajouter tous les index dans les éléments déjà 
-                % modifiés dans la liste des éléments déjà modifiés.
+            
+            
+            % Now we have to see if there are enough elements in the group
+            % If there are enough == minimal number given in argument ->ok, 
+            % If there are not enough, we can delete the element i which has  
+            % generated the group. The other elements in the group are not
+            % deleted as they could potentially belong to another group.
+            % other group.
+            if nb_same_pics_diff_channels >= nb_min_same_spike % si le pic est suffisamment détecté
+                % assign all values in the group to the average of the
+                % group.
+                % It is also necessary to add all the indexes in the elements already 
+                % modified items in the list of already modified items.
                 mean_val = mean(A(same_pics_indexes));
-                %mean_val = min(A(same_pics_indexes));
+                %mean_val = max(A(same_pics_indexes));
                 
                 %fprintf("pic détecté : i = %i , mean val = %f\n", mean_val);
                 %disp("index du pic");
                 %disp(same_pics_indexes);
-                for k=1:nb_same_pics
+                for k=1:nb_same_spike
                     %fprintf("index = %i:  %f -> %f\n", same_pics_indexes(k), A(same_pics_indexes(k)), mean_val);
                     A(same_pics_indexes(k)) = mean_val;
                     already_modified_index(already_modified_index_length+1) = same_pics_indexes(k);
                     already_modified_index_length = already_modified_index_length + 1; 
                 end            
 
-            else % sinon : effacer le pic car il n'est pas suffisamment détecté
+            else % otherwise: delete the spike because it is not sufficiently detected
                 A(i) = []; %suppression
                 B(i) = [];
-                l = size(A); % mise à jour de la taille du tableau
-                i = i - 1; % mise à jour de l'index courant
-                % mise à jour du tableau des valeurs déjà modifiées
+                l = size(A); % update of the table size
+                i = i - 1; % update of the current index
+                % update of the table of values already modified
                 for k=1:already_modified_index_length
                     already_modified_index(k) = already_modified_index(k) - 1;
                 end
